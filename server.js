aed24a22736bf5f9f43d4e59e631748ca4e526e5
Cait Sidener <42299666+cait-sidener@users.noreply.github.com>
2019-02-28 18:12:41 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+var setHtmlRoutes = require('./routing/htmlRoutes');
+var setApiRoutes = require("./routing/apiRoutes");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Access to CSS files
+app.use(express.static(path.join(__dirname, "./app/public")));
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+
+setHtmlRoutes(app);
+setApiRoutes(app);
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
